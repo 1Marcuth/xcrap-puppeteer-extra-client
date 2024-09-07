09a87c66d8a4f75663eaa086f774e1d2e4c42686
Marcuth <thiago.playgames15@gmail.com>
2024-09-07 20:59:42 -0300

    Adicionando Xcrap nas dependências

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,14 @@
 import { PuppeteerClientOptions } from "xcrap/dist/clients/puppeteer.client"
-import BaseClient, { Client } from "xcrap/dist/clients/base.client"
-import puppeteer, { PuppeteerExtraPlugin } from "puppeteer-extra"
-import { Browser, Page as PuppeteerPage } from "puppeteer"
-import PageSet from "xcrap/dist/page-set"
-import Page from "xcrap/dist/page"
+import puppeteer,  { PuppeteerExtraPlugin } from "puppeteer-extra"
+import { PuppeteerClient } from "xcrap"
 
-export type PuppeteerExtraProxy = string
-
-export type PuppeteerExtraClientOptions = Omit<PuppeteerClientOptions & {
+export type PuppeteerExtraClientOptions = PuppeteerClientOptions & {
     plugins?: PuppeteerExtraPlugin[]
-}, "targetFilter">
-
-export type GetMethodOptionActionsPropItem = (page: PuppeteerPage) => any | Promise<any>
-
-export type GetMethodOptions = {
-    url: string
-    actions?: GetMethodOptionActionsPropItem[]
-    javaScriptEnabled?: boolean
 }
 
-export type GetAllMethodOptions = GetMethodOptions[]
-
-class PuppeteerExtraClient extends BaseClient<PuppeteerExtraProxy> implements Client {
-    public readonly options: PuppeteerExtraClientOptions
-    private browser?: Browser
-
+class PuppeteerExtraClient extends PuppeteerClient {
     public constructor(options: PuppeteerExtraClientOptions = {}) {
         super(options)
-        this.options = options
-        this.browser = undefined
 
         if (options.plugins) {
             for (const plugin of options.plugins) {
@@ -37,11 +17,7 @@ class PuppeteerExtraClient extends BaseClient<PuppeteerExtraProxy> implements Cl
         }
     }
 
-    public usePlugin(plugin: PuppeteerExtraPlugin): void {
-        puppeteer.use(plugin)
-    }
-
-    private async initBrowser(): Promise<void> {
+    protected async initBrowser(): Promise<void> {
         const puppeteerArguments: string[] = []
 
         if (this.proxy) {
@@ -57,79 +33,14 @@ class PuppeteerExtraClient extends BaseClient<PuppeteerExtraProxy> implements Cl
         }
 
         this.browser = await puppeteer.launch({
-           ...this.options,
+            ...this.options,
             args: puppeteerArguments,
             headless: this.options.headless ? "shell" : false
         })
     }
 
-    private async closeBrowser(): Promise<void> {
-        if (this.browser) {
-            await this.browser.close()
-            this.browser = undefined
-        }
-    }
-
-    public async get(urlOrOptions: string | GetMethodOptions): Promise<Page> {
-        let url = typeof urlOrOptions === "string" ? urlOrOptions : urlOrOptions.url
-
-        if (!this.browser) {
-            await this.initBrowser()
-        }
-
-        const currentCorsProxyUrl = typeof this.corsProxyUrl === "function" ?
-            this.corsProxyUrl() :
-            this.corsProxyUrl
-
-        const currentUserAgent = typeof this.userAgent === "function" ?
-            this.userAgent() :
-            this.userAgent
-
-        const page = await this.browser!.newPage()
-
-        if (currentUserAgent) {
-            page.setUserAgent(currentUserAgent)
-        }
-
-        if (typeof urlOrOptions !== "string" && urlOrOptions.javaScriptEnabled) {
-            page.setJavaScriptEnabled(urlOrOptions.javaScriptEnabled ? true : false)
-        }
-
-        await page.goto(`${currentCorsProxyUrl ?? ""}${url}`)
-
-        if (typeof urlOrOptions !== "string") {
-            const actions = urlOrOptions.actions ?? []
-
-            for (const action of actions) {
-                await action(page)
-            }
-        }
-
-        const content = await page.content()
-        await page.close()
-
-        return new Page(content)
-    }
-
-    public async getAll(urlsOrOptions: string[] | GetAllMethodOptions): Promise<PageSet> {
-        if (!this.browser) {
-            await this.initBrowser()
-        }
-
-        const pageSet = new PageSet()
-
-        for (const urlOrOption of urlsOrOptions) {
-            const page = await this.get(urlOrOption)
-            pageSet.push(page)
-        }
-
-        return pageSet
-    }
-
-    public async close(): Promise<void> {
-        if (this.browser) {
-            await this.closeBrowser()
-        }
+    public usePlugin(plugin: PuppeteerExtraPlugin): void {
+        puppeteer.use(plugin)
     }
 }
 
